9597e8aafe70a3f12fea8ddbe47745bac8272eee
unknown <noel.osullivan2@ext.soprasteria.com>
2021-02-17 22:40:12 +0100

    NOS - No idea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import logo from './images/logo-large.png';
+import redButton from './images/red-button.png';
+import zip from './images/zip.png';
+import yellowButton from './images/yellow-button.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <img src={logo} className="central-logo" alt="logo" />
+      </div>
+
+      {/* <img src={redButton} className="red-button" alt="logo" />
+      <div className="rubriques">Les tissues</div>
+      
+
+      <img src={zip} className="zip" alt="logo" />
+      <img src={yellowButton} className="yellow-button" alt="logo" /> */}
+
     </div>
   );
 }
