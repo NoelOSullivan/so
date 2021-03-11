52a3cb00f7fc7f3e5ad080cae2ffde7a09723e45
unknown <noel.osullivan2@ext.soprasteria.com>
2021-03-11 23:25:12 +0100

    NOS - Animation Logo done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './images/logo-large.png';
+import logoHole from './images/logo-large-hole.png';
 import redButton from './images/red-button.png';
+import background from './images/background.jpg';
 import zip from './images/zip.png';
 import yellowButton from './images/yellow-button.png';
 import './App.css';
@@ -7,19 +9,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <div className="App-header">
-        <img src={logo} className="central-logo" alt="logo" />
+        <img src={logo} className="central-logo anim-logo" alt="logo" />
+        <img src={logoHole} className="central-logo anim-logo-hole" alt="logo" />
       </div>
-
-      {/* <img src={redButton} className="red-button" alt="logo" />
-      <div className="rubriques">Les tissues</div>
       
-
-      <img src={zip} className="zip" alt="logo" />
-      <img src={yellowButton} className="yellow-button" alt="logo" /> */}
-
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+{/* <div className="App-header">
+        <img src={logo} className="central-logo" alt="logo" />
+      </div>
+
+      <img src={redButton} className="red-button" alt="logo" />
+      <div className="rubriques">Les tissues</div>
+      
+      <img src={zip} className="zip" alt="logo" />
+      <img src={yellowButton} className="yellow-button" alt="logo" /> */}
