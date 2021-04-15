26bc62f1b68dda725b250dc0ee7bc23c7c28e426
unknown <noel.osullivan2@ext.soprasteria.com>
2021-04-15 18:50:07 +0200

    NOS - No idea

--- src/App.js
+++ src/App.js
@@ -15,24 +15,17 @@ function App() {
         <img src={logo} className="central-logo anim-logo" alt="logo" />
         <img src={logoHole} className="central-logo anim-logo-hole" alt="logo" />
       </div>
+
+      <div className="buttons">
+        <img src={zip} className="zip" alt="zip" />
+        <img src={zip} className="zip" alt="zip" />
+        <img src={zip} className="zip" alt="zip" />
+        <img src={zip} className="zip" alt="zip" />
+        <img src={zip} className="zip" alt="zip" />
+      </div>
       
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-{/* <div className="App-header">
-        <img src={logo} className="central-logo" alt="logo" />
-      </div>
-
-      <img src={redButton} className="red-button" alt="logo" />
-      <div className="rubriques">Les tissues</div>
-      
-      <img src={zip} className="zip" alt="logo" />
-      <img src={yellowButton} className="yellow-button" alt="logo" /> */}
