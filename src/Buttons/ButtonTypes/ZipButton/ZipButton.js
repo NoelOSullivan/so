bb3750bf36e53fac6eb3a212ed420be8bd6c99e3
unknown <noel.osullivan2@ext.soprasteria.com>
2021-06-25 21:20:28 +0200

    NOS - Button placement

--- src/Buttons/ButtonTypes/ZipButton/ZipButton.js
+++ src/Buttons/ButtonTypes/ZipButton/ZipButton.js
@@ -1,4 +1,8 @@
 import zip from './images/zip1.png';
+import zipLeft from './images/zipLeft.png';
+import zipRight from './images/zipRight.png';
+import zipOpenLeft from './images/zipOpenLeft.png';
+import zipOpenRight from './images/zipOpenRight.png';
 import './ZipButton.css';
 
 function ZipButton(props) {
@@ -6,12 +10,19 @@ function ZipButton(props) {
     function openContent() {
         props.changeContent(props.id);
     }
+
+    const zipOpen = props.type === "left" ? zipOpenLeft : zipOpenRight;
+    const zipOpenClass = props.type === "left" ? "zip-open-left"  : "zip-open-right" ;
+    const zip = props.type === "left" ? zipLeft : zipRight;
+    const zipClass = props.type === "left" ? "zip-left"  : "zip-right" ;
+    const etiquetteClass = props.type === "left" ? "etiquette etiquette-left"  : "etiquette etiquette-right" ;
     
     return (
-        <button onClick={openContent}>
-            <img src={zip} className="zip" alt="zip" />
-            <div className="etiquette">{props.name}</div>
-        </button>
+        <div className="zip-button-container" style={{top:props.top, bottom:props.bottom}} onClick={openContent}>
+            <img src={zipOpen} className={zipOpenClass} alt="zipOpen" />
+            <img src={zip} className={zipClass} alt="zip" />
+            <div className={etiquetteClass}>{props.name}</div>
+        </div>
     );
 }
 
--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -4,13 +4,16 @@ import './Buttons.css';
 function Buttons(props) {
     return (
         <div className="buttons">
-            <ZipButton id="1" name="Sacs" changeContent={props.changeContent} />
-            <ZipButton id="2" name="Cuisine" changeContent={props.changeContent} />
-            <ZipButton id="3" name="Salle de Bain" changeContent={props.changeContent} />
-            <ZipButton id="4" name="Zero dechets" changeContent={props.changeContent} />
-            <ZipButton id="5" name="Mode" changeContent={props.changeContent} />
-            <ZipButton id="6" name="Retouche" changeContent={props.changeContent} />
-            <ZipButton id="7" name="Contact" changeContent={props.changeContent} />
+            <ZipButton type="left" top="-20px" left="0px" id="1" name="Les Sacs" changeContent={props.changeContent} />
+            <ZipButton type="right" top="30px" right="0px" id="2" name="Mode" changeContent={props.changeContent} />
+            <ZipButton type="left" top="80px" left="0px" id="3" name="Cuisine" changeContent={props.changeContent} />
+            <ZipButton type="right" top="130px" right="0px" id="4" name="Salle de bains" changeContent={props.changeContent} />
+            {/* <ZipButton type="left" top="310px" right="0px" id="4" name="Salle de bains" changeContent={props.changeContent} /> */}
+            {/* <ZipButton className="zip zip3" id="3" name="Salle de Bain" changeContent={props.changeContent} />
+            <ZipButton className="zip zip4" id="4" name="Zéro dechets" changeContent={props.changeContent} />
+            <ZipButton className="zip zip5" id="5" name="Mode" changeContent={props.changeContent} />
+            <ZipButton className="zip zip6" id="6" name="Retouche" changeContent={props.changeContent} />
+            <ZipButton className="zip zip7" id="7" name="Contact" changeContent={props.changeContent} /> */}
         </div>
     );
 }
